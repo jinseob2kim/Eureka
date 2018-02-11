eec348be12482f173ce02cea8929120e990cdeeb
jinseob2kim <jinseob2kim@gmail.com>
2018-02-11 17:48:04 +0900

    ERK

---
+++ contracts/Eureka.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.13;
+
+import 'zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
+
+contract Eureka is MintableToken {
+  string public name = "Eureka";
+  string public symbol = "ERK";
+  uint8 public decimals = 18;
+}
+
+
+
