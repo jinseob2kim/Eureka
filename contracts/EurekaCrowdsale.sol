7f5426d0d3664f29dd27e4122d4d08b508d23833
jinseob2kim <jinseob2kim@gmail.com>
2018-02-12 21:55:53 +0900

    RESET

--- contracts/EurekaCrowdsale.sol
+++ contracts/EurekaCrowdsale.sol
@@ -5,7 +5,12 @@ import 'zeppelin-solidity/contracts/crowdsale/Crowdsale.sol';
 
 
 contract EurekaCrowdsale is Crowdsale {
-
+  
+  // startTime : crowdSale 시작일
+  // endTime :  종료일
+  // rate : 이더-코인 교환비. 토큰 발행량 = 이더 x rate
+  // wallet : crowdSale에 지급되는 이더를 받을 지갑
+ 
   function EurekaCrowdsale(uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet) 
     Crowdsale(_startTime, _endTime, _rate, _wallet) {          
   }
