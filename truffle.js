1480c7f691162c18b4891cae54021ac86d70167a
jinseob2kim <jinseob2kim@gmail.com>
2018-02-13 10:44:49 +0900

    sabjil

--- truffle.js
+++ truffle.js
@@ -2,18 +2,26 @@ module.exports = {
   networks: {
     development: {
       host: "localhost",
-      port: 8545,
+      port: 9545,
       network_id: "*" // Match any network id
     },
      ropsten:  {
      network_id: 3,
      host: "localhost",
      port:  8545,
+     from: "0x662e52853f9832fc988E47B9a1F942f0ba06FF6e",
      gas:   2900000
-}
-  },
-   rpc: {
+    },
+    rinkeby: {
+      network_id: 4,
+      host: "localhost",
+      port: 8545,
+      from: "0x662e52853f9832fc988E47B9a1F942f0ba06FF6e",
+      gas: 4000000
+    },
+    rpc: {
  host: 'localhost',
  post:8080
    }
+  }
 };
